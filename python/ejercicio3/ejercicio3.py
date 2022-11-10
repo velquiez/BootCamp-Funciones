b1afb3ad38928991bad7295d570f62e3b8a3ebf9
velquiez <107650421+velquiez@users.noreply.github.com>
2022-11-10 08:42:44 -0500

    Update ejercicio3.py

--- python/ejercicio3/ejercicio3.py
+++ python/ejercicio3/ejercicio3.py
@@ -1,9 +1,8 @@
-print('Calcular el Indice de Masa Muscular - IMC')
-peso = float(input('Digite su peso en Kg, por favor: '))
-estatura = float(input('Ahora su estatura en mt, por ejemplo: <<1.67>>, digitela: '))
-
-import math
-imc = math.ceil(float(peso//estatura**2))
-print(f'su indice de masa corporal es {imc:.2f}')
-
-input('Precione una tecla para salir')
+print('Calcular el Indice de Masa Muscuar - IMC')
+peso = input('Digite su peso en Kg, por favor: ')
+estatura = input('Ahora su estatura en mt, por ejemplo: <<1.67>>, digitela: ')
+#import math
+#imc = math.ceil(float(peso)/float(estatura)**2)
+imc = float(peso)/float(estatura)**2
+print(f'Tu indice de masa corporal es {imc:.2f}')
+#input('Precione una tecla para salir')
