088a2bebbf3da41773a64dad6e2782d0a5d9c0c3
velquiez <107650421+velquiez@users.noreply.github.com>
2023-03-16 07:40:01 -0500

    Create contador_inverso.py

---
+++ python/ejercicio4/contador_inverso.py
@@ -0,0 +1,15 @@
+lista = []
+a = 100
+
+while (a >= 1):
+    lista.append(a)
+    a -=1
+    #print(a)
+    
+print(lista)
+
+#r = 100
+#a = 100
+#for i in range(a,r,-1):
+#   lista.append(a)
+#print(lista)
