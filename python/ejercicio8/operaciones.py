d5dc1558a07d4788daa352123de425cecf90baca
velquiez <107650421+velquiez@users.noreply.github.com>
2023-07-19 09:49:26 -0500

    Add files via upload

---
+++ python/ejercicio8/operaciones.py
@@ -0,0 +1,12 @@
+#Se crea modulo con operaciones basicas aritmetica
+def sumar(x,y):
+    return print('La suma es: ', x + y)
+
+def restar(x,y):
+    return print('La resta es: ', x - y)
+
+def multiplicar(x,y):
+    return print('La multiplicacion es: ', x * y)
+
+def dividir(x,y):
+    return print('La division es: ', x / y)
