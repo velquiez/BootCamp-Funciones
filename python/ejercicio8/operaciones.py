05d9692428890cac90a043415060f13e35945187
velquiez <velquiez@hotmail.com>
2023-07-20 17:39:13 -0500

    nuevos camvios
    cambios

--- python/ejercicio8/operaciones.py
+++ python/ejercicio8/operaciones.py
@@ -1,12 +1,3 @@
 #Se crea modulo con operaciones basicas aritmetica
-def sumar(x,y):
-    return print('La suma es: ', x + y)
-
-def restar(x,y):
-    return print('La resta es: ', x - y)
-
-def multiplicar(x,y):
-    return print('La multiplicacion es: ', x * y)
-
-def dividir(x,y):
-    return print('La division es: ', x / y)
+def aritmetica(x,y):
+    return x + y, x - y, x * y, x / y
--- python/ejercicio8/principal.py
+++ python/ejercicio8/principal.py
@@ -1,25 +1,15 @@
-#Ejecutar con python version 3.10 en adelante
-
-import operaciones as o
+import operacions as o
 
 def main():
 
   x = float(input('Digite el primer numero: '))
   y = float(input('Escriba el segundo numero: '))
-  print('-->> 1 = sumar ; 2 = restar ; 3 = multiplicar ; 4 = dividir') 
+  print('-->> 0 = sumar ; 1 = restar ; 2 = multiplicar ; 3 = dividir') 
   tipOperar = int(input('Escoja el tipo de operacion aritmetica: '))
 
-  match tipOperar:
-    case 1:
-       operar = o.sumar(x,y)
-    case 2:
-       operar = o.restar(x,y)
-    case 3:
-       operar = o.multiplicar(x,y)
-    case 4:
-       operar = o.dividir(x,y)
- 
+  operar = o.aritmetica(x,y)
+  print('La operacion seleccionada da: ', operar[tipOperar])
+  
 if __name__ == '__main__' :
   main()
-  
  
