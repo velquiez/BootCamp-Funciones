286c96afc3a323c670768b399b41c33cbd79f8b0
velquiez <velquiez@hotmail.com>
2023-08-03 21:58:07 -0500

    uso BD

---
+++ ensayoPython/CrearInterfaces/BD_verFunciones.py
@@ -0,0 +1,10 @@
+#import tkinter
+#from tkinter import ttk
+import sys
+import sqlite3
+
+#window = tkinter.Tk()
+#print(type(window))
+import pprint
+#pprint.pprint(dir(window)) #Para ver todas las funciones, metodos, clases que tiene tkinter
+pprint.pprint(dir(sqlite3))
